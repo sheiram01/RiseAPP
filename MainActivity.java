04467f645bb0a2b4044da241515255e81c7733b8
noramark <nrmarkus@gmail.com>
2017-08-08 11:38:48 -0400

    first commit profile

---
+++ MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a2019eleanorm.gwcrise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.call_senator).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialContactPhone("2022246542");
+            }
+        });
+    }
+
+    private void dialContactPhone(final String phoneNumber) {
+        startActivity(new Intent(Intent.ACTION_DIAL, android.net.Uri.fromParts("tel", phoneNumber, null)));
+    }
+}
